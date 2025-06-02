16c58d3826247faacd50e4722739a0f7d382047f
sintia mahlangu <sintiamahlangu@gmail.com>
2025-06-02 21:22:15 +0200

    Update database

---
+++ Survey/Migrations/20250602191820_RenameColumns.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Survey.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LikesBeefStirFry",
+                table: "Surveys");
+
+            migrationBuilder.DropColumn(
+                name: "LikesChickenStirFry",
+                table: "Surveys");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Surveys",
+                newName: "FullName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Surveys",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Surveys",
+                newName: "DateOfBirth");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Surveys",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Surveys",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Surveys",
+                newName: "Date");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LikesBeefStirFry",
+                table: "Surveys",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LikesChickenStirFry",
+                table: "Surveys",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
